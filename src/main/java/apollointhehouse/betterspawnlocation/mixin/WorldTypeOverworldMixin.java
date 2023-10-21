80cc0fbb5c48415e65a371323d6bd4e58b289302
Apollointhehouse <99405787+Apollointhehouse@users.noreply.github.com>
2023-10-21 23:14:32 +1300

    Fixed crash

--- src/main/java/apollointhehouse/betterspawnlocation/mixin/WorldTypeOverworldMixin.java
+++ src/main/java/apollointhehouse/betterspawnlocation/mixin/WorldTypeOverworldMixin.java
@@ -2,11 +2,15 @@ package apollointhehouse.betterspawnlocation.mixin;
 
 import net.minecraft.core.block.Block;
 import net.minecraft.core.world.World;
+import net.minecraft.core.world.config.season.SeasonConfig;
 import net.minecraft.core.world.type.WorldTypeOverworld;
+import net.minecraft.core.world.weather.Weather;
+import net.minecraft.core.world.wind.WindManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.Arrays;
 
@@ -14,16 +18,23 @@ import java.util.Arrays;
 public class WorldTypeOverworldMixin {
 
 	@Unique
-	int[] invalidSpawnBlocks = {
-		0,
-		Block.fluidLavaFlowing.id,
-		Block.fluidLavaStill.id,
-		Block.fluidWaterFlowing.id,
-		Block.fluidWaterStill.id,
-		Block.fire.id,
-		Block.spikes.id,
-		Block.algae.id
-	};
+	int[] invalidSpawnBlocks;
+
+	@Inject(method = "<init>", at = @At("TAIL"))
+	void init(String languageKey, Weather defaultWeather, WindManager windManager, SeasonConfig defaultSeasonConfig, CallbackInfo ci) {
+		invalidSpawnBlocks = new int[] {
+			0,
+			Block.fluidLavaFlowing.id,
+			Block.fluidLavaStill.id,
+			Block.fluidWaterFlowing.id,
+			Block.fluidWaterStill.id,
+			Block.fire.id,
+			Block.spikes.id,
+			Block.algae.id,
+			Block.cactus.id
+		};
+	}
+
 	@Inject(method = "isValidSpawn", at = @At("RETURN"), cancellable = true)
 	void isValidSpawn(World world, int x, int y, int z, CallbackInfoReturnable<Boolean> cir) {
 		cir.setReturnValue(
